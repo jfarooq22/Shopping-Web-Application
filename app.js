783e1ba3d9ee9a621c37dd6f0d40fa1228fa0013
Jawwad Farooq <jawwadfarooq22@yahoo.com>
2021-05-02 16:46:47 +0500

    Update app.js
    Removed Mongodb url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://Jawwad:root@shoppingwebappcluster-hjeej.mongodb.net/shop?retryWrites=true&w=majority';
+  'YOUR URI HERE';
 
 const app = express();
 const store = new MongoDBStore({
